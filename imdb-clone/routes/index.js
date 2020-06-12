225b9ba31c544d13c1f7076e8ecf149525e1d21e
roshangrewal <roshangrewal@hotmail.com>
2020-06-13 02:53:20 +0530

    local api endpoints

--- imdb-clone/routes/index.js
+++ imdb-clone/routes/index.js
@@ -3,10 +3,18 @@ var router = express.Router();
 const request = require('request');
 const app = require('../app');
 
-const apiKey = '1fb720b97cc13e580c2c35e1138f90f8';
-const apiBaseUrl = 'http://api.themoviedb.org/3';
-const nowPlayingUrl = `${apiBaseUrl}/movie/now_playing?api_key=${apiKey}`;
+// THEMOVIEDB API
+// const apiKey = '1fb720b97cc13e580c2c35e1138f90f8';
+// const apiBaseUrl = 'http://api.themoviedb.org/3';
+// const nowPlayingUrl = `${apiBaseUrl}/movie/now_playing?api_key=${apiKey}`;
+// const imageBaseUrl = 'http://image.tmdb.org/t/p/w300';
+
+// LOCAL API imdb-clone-local-api
+const apiKey = 123456789;
+const apiBaseUrl = 'http://localhost:3030';
+const nowPlayingUrl = `${apiBaseUrl}/most_popular?api_key=${apiKey}`;
 const imageBaseUrl = 'http://image.tmdb.org/t/p/w300';
+
 
 router.use((req, res, next) => {
   res.locals.imageBaseUrl = imageBaseUrl;
