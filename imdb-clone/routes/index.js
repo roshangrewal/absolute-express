36943775530e630d8cb3ec723102779e1fa8450d
roshangrewal <roshangrewal@hotmail.com>
2020-06-13 17:57:12 +0530

    activated THEMOVIEDB API

--- imdb-clone/routes/index.js
+++ imdb-clone/routes/index.js
@@ -4,16 +4,16 @@ const request = require('request');
 const app = require('../app');
 
 // THEMOVIEDB API
-// const apiKey = '1fb720b97cc13e580c2c35e1138f90f8';
-// const apiBaseUrl = 'http://api.themoviedb.org/3';
-// const nowPlayingUrl = `${apiBaseUrl}/movie/now_playing?api_key=${apiKey}`;
-// const imageBaseUrl = 'http://image.tmdb.org/t/p/w300';
-
-// LOCAL API imdb-clone-local-api
-const apiKey = 123456789;
-const apiBaseUrl = 'http://localhost:3030';
-const nowPlayingUrl = `${apiBaseUrl}/most_popular?api_key=${apiKey}`;
+const apiKey = '1fb720b97cc13e580c2c35e1138f90f8';
+const apiBaseUrl = 'http://api.themoviedb.org/3';
+const nowPlayingUrl = `${apiBaseUrl}/movie/now_playing?api_key=${apiKey}`;
 const imageBaseUrl = 'http://image.tmdb.org/t/p/w300';
+
+// // LOCAL API imdb-clone-local-api
+// const apiKey = 123456789;
+// const apiBaseUrl = 'http://localhost:3030';
+// const nowPlayingUrl = `${apiBaseUrl}/most_popular?api_key=${apiKey}`;
+// const imageBaseUrl = 'http://image.tmdb.org/t/p/w300';
 
 
 router.use((req, res, next) => {
